425e4fe93aa7ae02d8f33c4d92187f8dc12bf8f3
GK <GK@Cytron>
2019-01-21 19:58:31 +0530

    refresh

--- signup.php
+++ signup.php
@@ -18,7 +18,7 @@
  *                Priya Patel
  * @filename      index.php
  * @begin         2018-12-21
- * @update        2019-01-05
+ * @update        2019-01-21
  */
 
 require("include/config.php");
@@ -93,7 +93,7 @@ require("include/db.php");
             <!--<h2 class="mx-auto mb-5"></h2>-->
             <div class="sinup-box card">
                 <div class="card-body">
-                    <form class="form-horizontal form-material" id="signup" action="include/add-user.php" method="post">
+                    <form class="form-horizontal form-material" id="signupForm" action="include/add-user.php" method="post">
                         <h3 class="text-center m-b-20">Sign Up</h3>
                         <div class="form-group">
                         <?php
@@ -236,23 +236,99 @@ require("include/db.php");
     <script src="asset/js/stylish-portfolio.min.js"></script>
 
     <script src="asset/js/jqBootstrapValidation.js"></script>
-    <script src="asset/js/contact_me.js"></script>
+    <!--script src="asset/js/contact_me.js"></script-->
 
     <!--Wave Effects -->
-    <script src="asset/dist/js/waves.js"></script>
+    <script src="/asset/dist/js/waves.js"></script>
     <!--Menu sidebar -->
-    <script src="asset/dist/js/sidebarmenu.js"></script>
+    <script src="/asset/dist/js/sidebarmenu.js"></script>
     <!--stickey kit -->
-    <script src="assets/node_modules/sticky-kit-master/dist/sticky-kit.min.js"></script>
-    <script src="assets/node_modules/sparkline/jquery.sparkline.min.js"></script>
+    <script src="/assets/node_modules/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="/assets/node_modules/sparkline/jquery.sparkline.min.js"></script>
     <!--Custom JavaScript -->
-    <script src="asset/dist/js/custom.min.js"></script>
+    <script src="/asset/dist/js/custom.min.js"></script>
     <!-- ============================================================== -->
     <!-- This page plugins -->
     <!-- ============================================================== -->
-    <script src="asset/dist/js/pages/jasny-bootstrap.js"></script>
+    <script src="/asset/dist/js/pages/jasny-bootstrap.js"></script>
     <!-- Custom Theme JavaScript -->
     <script>
+$(function() {
+
+  $("#signupForm input,#signupForm select").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var phone = $("input#phone").val();
+      var message = $("textarea#message").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $.ajax({
+        url: "../../include/contact_me.php",
+        type: "POST",
+        data: {
+          name: name,
+          phone: phone,
+          email: email,
+          message: message
+        },
+        cache: false,
+        success: function() {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        complete: function() {
+          setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
+
     $('#usertype').change(function(e) {
         if($('#usertype').val() == 'freelancer') {
             $("#freelancerForm").show();
