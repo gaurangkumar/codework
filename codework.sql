ddd3fbf2a74bc9d8c9e45ffb642fa9bfe3126da4
Gaurang Kumar <gaurangkumarp@gmail.com>
2019-03-06 12:15:16 +0530

    sql

---
+++ codework.sql
@@ -0,0 +1,238 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 06, 2019 at 07:20 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `codework`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `email`, `password`, `date`) VALUES
+(1, 'Admin', 'abc@abc.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '2019-01-21 10:38:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `cid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`cid`, `name`, `email`, `password`, `date`) VALUES
+(2, 'client1', 'client@abc.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '2019-01-28 16:42:30'),
+(3, 'abc', 'abc@abc.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '2019-02-06 07:29:18'),
+(4, '', 'NULL', '', '2019-02-08 16:20:42'),
+(5, '', '', '', '2019-02-08 16:21:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `fbid` int(11) NOT NULL,
+  `name` varchar(120) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `msg` varchar(500) NOT NULL,
+  `is_read` tinyint(1) NOT NULL DEFAULT '0',
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `freelancer`
+--
+
+CREATE TABLE `freelancer` (
+  `fid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `lang` varchar(10) NOT NULL,
+  `cv` varchar(50) NOT NULL,
+  `id_proof` varchar(50) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `freelancer`
+--
+
+INSERT INTO `freelancer` (`fid`, `name`, `email`, `password`, `lang`, `cv`, `id_proof`, `date`) VALUES
+(1, 'freelancer', 'free@abc.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'php', 'user_data/2019-01-28-22-13-29-ba683e9ab36398d2c27b', 'user_data/2019-01-28-22-13-29-Brie-Larson-Actress0', '2019-01-28 16:43:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_prj`
+--
+
+CREATE TABLE `post_prj` (
+  `pid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `detail` text NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `lang` varchar(20) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `fid` int(11) DEFAULT NULL,
+  `status` varchar(20) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `paid` tinyint(1) NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `post_prj`
+--
+
+INSERT INTO `post_prj` (`pid`, `name`, `detail`, `category`, `file`, `lang`, `cid`, `fid`, `status`, `cost`, `paid`, `date`) VALUES
+(1, 'website', 'ababab', 'website', '', 'php', 3, 0, '', 5000, 0, '2019-02-06 13:07:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_req`
+--
+
+CREATE TABLE `post_req` (
+  `rid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `fid` int(11) NOT NULL,
+  `msg` varchar(255) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`cid`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`fbid`);
+
+--
+-- Indexes for table `freelancer`
+--
+ALTER TABLE `freelancer`
+  ADD PRIMARY KEY (`fid`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `post_prj`
+--
+ALTER TABLE `post_prj`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `post_req`
+--
+ALTER TABLE `post_req`
+  ADD PRIMARY KEY (`rid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `fbid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `freelancer`
+--
+ALTER TABLE `freelancer`
+  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `post_prj`
+--
+ALTER TABLE `post_prj`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `post_req`
+--
+ALTER TABLE `post_req`
+  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
