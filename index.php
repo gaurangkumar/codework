ab25374369571144b056c41858ac51e0c54c484a
gaurangkumar <gaurangkumarp@gmail.com>
2020-10-10 10:17:08 +0530

    font awesome fixed

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ require("include/db.php");
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
